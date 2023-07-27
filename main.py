dfdae5c8fee6636d5ccdf40487f4c0ff3b39bd0d
Paul Murley <pmmurley830@gmail.com>
2023-07-26 19:20:24 -0500

    Remove analysis directory

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import ootp_data_readers
+import ootp_data_readers as p
 
 
 
 def test_read_file():
-    cities_data = ootp_data_readers.cities.parse('data/2025/cities.csv')
+    cities_data = p.cities.parse('data/2025/cities.csv')
     for city_id, city in cities_data.items():
         print(city)
 
